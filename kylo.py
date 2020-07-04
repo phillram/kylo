5fdaf11f4941bd73a862fd80fa865ee83506af69
phillram <14913130+phillram@users.noreply.github.com>
2020-07-04 15:27:22 -0700

    Broke functions into separate file

--- kylo.py
+++ kylo.py
@@ -1,20 +1,22 @@
 ####################################################################
+# Kylo -- A hopefully useful tool for Rollbar
+####################################################################
+
+####################################################################
 # Importing requirements 
 ####################################################################
-import os, random, urllib3, json
+from kylo_functions import *
+
+import os, random
 from pyngrok import ngrok
-from flask import Flask, request, got_request_exception, render_template, Markup, flash
+from flask import Flask, request, got_request_exception, render_template
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
 from flask_nav import Nav
 from flask_nav.elements import *
-from wtforms import StringField, TextField, SubmitField, SelectField
-from wtforms.validators import DataRequired
 
 # Logging imports you may not need
 # import logging, certifi
 # import ssl as ssl_lib
-
 
 ####################################################################
 # Initializing Flask
@@ -37,7 +39,7 @@ app.config['SECRET_KEY'] = str(random.randint(0,100000000000))
 def mynavbar():
     return Navbar(
         'Kylo',
-        View('Home', 'index'),
+        View('Create Item', 'index'),
     )
 
 # Favicon routing
@@ -72,74 +74,8 @@ def index():
     # Renders the page
     return render_template('index.html', form = form)
 
-
 ####################################################################
-# Functions
-####################################################################
-# Use flask-wtf to create the fields for the form generation
-class create_message_form(FlaskForm):
-    # Fields for user to fill out
-    post_server_token = StringField('Your post_server_item token',
-        [ DataRequired() ]
-    )
-    post_client_token = StringField('Your post_client_item token',
-    )
-    rollbar_environment = StringField('Environment',
-    )
-    message_type = SelectField( 'Message Type',
-        choices=[ ('critical', 'Critical'), ('debug', 'Debug'),('error', 'Error'),('info', 'Info'),('warning', 'Warning') ]
-    )
-    rollbar_message = StringField('Message to send',
-    )
-
-
-# Use the Rollbar API to perform an action
-# Method is the API method
-# Currently it's only creating items
-# I'll likely edit this to create an endpoint
-
-def perform_api_request(method, endpoint, post_client_token, post_server_token, 
-    rollbar_environment, message_type, rollbar_message):
-
-    # Building the API call
-    url = 'https://api.rollbar.com/api/1/' + endpoint + '/'
-
-    header = {
-            'X-Rollbar-Access-Token' : post_server_token,
-        }
-
-    body = json.dumps({
-            'data':{
-                'environment': rollbar_environment,
-                'level': message_type,
-                'body' : {
-                    'message' : {
-                        'body': rollbar_message,
-                        },
-                },
-            }
-        })
-
-    # Add the information to display a cURL  in the UI
-    flash('Your not fully implemented to be API call:\n' + str(url) + str(header) + str(body) + '\n', 'api_request')
-
-    # Taking information from the form and creating a new item via Rollbar API
-    rollbar_response_raw = urllib3.PoolManager().request(method, url, headers=header, body=body)
-
-    # Parsing the API result above and saves the response to display in UI
-    rollbar_response = json.loads(rollbar_response_raw.data.decode('utf-8'))
-
-    # Check if it's an error response or not, then display it in the UI
-    if rollbar_response['err'] == 0 :
-        flash_message = Markup(rollbar_response['result'])
-    else:
-        flash_message = Markup(rollbar_response['message'])
-    
-    # There shouldn't be a case where flash_message isn't set (hopefully!)
-    flash('Response from Rollbar: ' + flash_message, 'api_response')
-
-####################################################################
-# Start Maul and run flask on set port
+# Start Kylo and run flask on set port
 ####################################################################
 if __name__ == '__main__':
     port = '5066'
